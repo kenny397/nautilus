df1aa98cba88ab5acf854d548640d74fd7c02b04
kenny <kenny397@naver.com>
2022-10-14 22:36:47 +0900

    test : Authentication test코드 추가

--- domain/src/test/kotlin/com/jun/nautilus/domain/impl/AuthenticatorImplTest.kt
+++ domain/src/test/kotlin/com/jun/nautilus/domain/impl/AuthenticatorImplTest.kt
@@ -1,5 +1,24 @@
 package com.jun.nautilus.domain.impl
 
+import com.jun.nautilus.domain.AuthManager
+import com.jun.nautilus.domain.AuthRepository
+import com.jun.nautilus.domain.Authenticator
+import com.jun.nautilus.domain.AuthenticatorTest
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 
-internal class AuthenticatorImplTest
+internal class AuthenticatorImplTest: AuthenticatorTest{
+
+    override lateinit var sut: Authenticator
+    override lateinit var authManager: AuthManager
+
+    lateinit var authRepository: AuthRepository
+
+    @BeforeEach
+    fun setUp(){
+        authRepository=AuthRepositoryImpl()
+        authManager= AuthManagerImpl(AuthFactoryImpl(),authRepository,Sha256Encoder())
+        sut= AuthenticatorImpl(authRepository,Sha256Encoder())
+
+    }
+}
